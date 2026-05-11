29735831e2474030a5af61fe66c47e0c43a0189c
SEBASTIAN <sebastian.chicata@gmail.com>
2026-05-11 15:36:25 -0500

    Creación del log_model y prueba

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,28 @@
-import 'dotenv/config'
 import { Server } from './presentation/server'
+import { LogModel, MongoDatabase } from './data/mongodb'
+import { envs } from './config/plugins/envs.plugin'
 
 (async() => {
     main()
-})() 
+})()
 
-function main() {
+async function main() {
 
-    Server.start()
+    await MongoDatabase.connect({
+        mongoUrl: envs.MONGO_URL,
+        dbName: envs.MONGO_DB_NAME
+    })
+
+    // Crear un registro 
+    /* const newLog = await LogModel.create({
+        message:'Test message from MongoDB',
+        origin:'App.ts',
+        level: 'low'
+    })*/
+
+    /*await newLog.save();
+    console.log('Log creado:', newLog);*/
+
+    //Server.start()
     //console.log( envs )
 }
